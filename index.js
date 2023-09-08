5b02fb507f308aa8c507dafeb8da8fced00dec80
Austin Olijar <Austin.Olijar@gmail.com>
2023-09-08 11:41:11 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ modal = document.querySelector(".modal");
 btn.addEventListener("click", () => {
   modal.classList.remove("active-modal");
 });
+
+const queryString = window.location.search;
+console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+const modalStatus = urlParams.get("modal");
+
+console.log(modalStatus);
+
+if (modalStatus === "active") {
+  modal.classList.add("active-modal");
+  document.querySelector(
+    ".params"
+  ).innerText = `URL Query String: ${queryString}`;
+}
